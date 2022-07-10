0ebf325c6563e40209752083a5786260c3934c5f
zshrestha2 <99230624+zoyace-shrestha@users.noreply.github.com>
2022-07-10 13:41:43 -0500

    README added

---
+++ README.md
@@ -0,0 +1,13 @@
+# The FLip
+
+## Demo
+
+![IMG!](img/flip.jpg)
+
+## Description
+
+Basic Java program that takes an input and converts the alphabet lowercase to uppercase and vice versa.
+
+## Language
+
+-Java
--- src/LetterFlip.java
+++ src/LetterFlip.java
@@ -1,12 +1,3 @@
-/**
- * This creates a window with a field for entering text, and associated
- * EventButton object for responding to user actions. The program will
- * take input in DNA format and generate the DNA complement, along with a count
- * of each base occurring. Non-DNA input results in an error message.
- *
- * @author D. Mathias
- */
-
 import java.awt.Color;
 import javax.swing.*;
 
